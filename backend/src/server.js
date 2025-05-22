0405833ad9a3497d4f70aed2d2ba1d36eed16053
Ederson Moraes <eder.israel.moraes@gmail.com>
2025-05-23 00:10:33 +0100

    created all routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const routes = require('./routes')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+dotenv.config()
 
 const app = express()
-
-
 app.use(express.json())
 
 app.use(routes)
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-}
-)
+mongoose.connect(process.env.MONGO_URI, {
+}).then(() => {
+    console.log('MongoDB connected')
+}).catch(err => {
+    console.error('MongoDB connection error:', err)
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
